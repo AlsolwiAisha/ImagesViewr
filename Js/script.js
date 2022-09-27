ba8d863233a262a43e9e7413f30ea84276cd9dee
AlsolwiAisha <aisha.alsolwi@gmail.com>
2022-09-27 22:25:17 +0300

    update Code

--- Js/script.js
+++ Js/script.js
@@ -1,32 +1,58 @@
+let ConStr;
 
+function getHtmlCode(Item){
+    return   `<div class="singlePhoto">
+    <div class="myPhoto">
+    <img src="${Item.urls.small_s3}" alt="${Item.id}" />
+    </div>
+    <div class="username">
 
-let div=document.querySelector(".imags");
+    <span style='color:${Item.color}'>${Item.user.name}</span>
+    <div class="statistics">
+
+    <div class="likesAndDownloads">
+    <img width="20" src="images/favorite_FILL0_wght400_GRAD0_opsz48.svg" />
+    <span>${Item.likes}</span></div>
+
+     <div class="likesAndDownloads">
+     <img width="20" src="images/file_download_FILL0_wght400_GRAD0_opsz48.svg" />
+     <span>${Item.downloads}</span></div>
+
+     </div>
+
+    </div>
+    </div>`;
+}
 async function getResponse() {
     
     let txt=document.querySelector("#text_").value;
 let cont=document.querySelector("#count").value;
-    console.log(txt);
-	let response = await fetch(`https://api.unsplash.com/search/photos/?query=${txt}&per_page=${cont}&client_id=nzkXeb3JiUd1iCWYpQBHAD91vCQ0UeQW00PO5iEx7Uk`);
-	if (!response.ok) {
-		throw new Error(`HTTP error! status: ${response.status}`);
-	}
-    let child = div.lastElementChild;  
-    while (child) { 
-        div.removeChild(child); 
-        child = div.lastElementChild; 
-    } 
-	let data = await response.json();
-    data.results.map((item) => {
-        let divSearch=document.createElement("div");
-        let element=document.createElement("img");
-        let elementName=document.createElement("h3");
-        element.src=item.urls.small;
-        elementName.innerHTML=item.user.name;
-        div.appendChild(divSearch)
-        divSearch.appendChild(element)
-        divSearch.appendChild(elementName)
-       
-});
-    
- // document.querySelector("#cu").innerHTML=`${data.total}`
+if(!txt && !cont)
+{
+     ConStr="https://api.unsplash.com/photos/random/?count=10&client_id=nzkXeb3JiUd1iCWYpQBHAD91vCQ0UeQW00PO5iEx7Uk";
 }
+else
+{
+ConStr=`https://api.unsplash.com/search/photos/?query=${txt}&per_page=${cont}&client_id=nzkXeb3JiUd1iCWYpQBHAD91vCQ0UeQW00PO5iEx7Uk`;
+}
+
+ let response = await fetch(ConStr);
+ if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+}
+let data= await response.json();
+ let Photos;
+   if(!txt && !cont){
+     Photos= data.map((item) => {
+        return  getHtmlCode(item);
+       
+});}
+else{ 
+    Photos= data.results.map((item) => {
+    
+        return  getHtmlCode(item);   
+});}
+document.querySelector(".imags").innerHTML = Photos.join("");
+
+}
+getResponse();
